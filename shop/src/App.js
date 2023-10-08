6194c9e5e32fd4953e3a6c1cafea00aa57bebcaa
dlfdydwlrshehdwk <fkrmsk147@naver.com>
2023-10-09 03:52:06 +0900

    2

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable */
+// warning 메시지 안뜨게 해주는 기능
+
+import './assets/scss/style.scss';
+import Header from './components/Header';
+import Footer from './components/Footer';
+// import React from 'react';
+import { useState } from 'react';
 
 function App() {
+  let [item,setItem] = useState(['감자','고구마','당근']);
+  let [good,setGood] = useState([0,0,0])
+
+  function goodUp(){
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <div className='container'>
+        <ul>
+          <li>
+            <div>{ item[0] }</div>
+            <div onClick={()=>{
+              let copy
+              copy = good;
+              copy[0] = copy[0] + 1;
+              setGood(copy)
+            }}>좋아요{good[0]}</div>
+          </li>
+          
+        </ul>
+      </div>
+      <Footer />
     </div>
   );
 }
