29e486f82aee85bf9e787323993415a614b4ae72
dlfdydwlrshehdwk <fkrmsk147@naver.com>
2023-10-09 16:47:26 +0900

    1

--- shop/src/components/component/List.js
+++ shop/src/components/component/List.js
@@ -1,4 +1,6 @@
 import { useEffect, useState } from 'react'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStarHalf } from "@fortawesome/free-solid-svg-icons";
 // 이미지태그를 쓸거면 img태그에서 경로를 지정하는게 아닌
 // import 를 사용하던가 public에 이미지를 놓고 경로를 지정해야함 
 import img1 from '../../assets/images/item1.PNG'
@@ -15,8 +17,12 @@ function List (props) {
             <div>
                 <img src={img1}></img>
             </div>
-            <ul className='star'>
-            </ul>
+            <div className='star'>
+                <span>
+                <FontAwesomeIcon icon={faStarHalf} style={{color: "#ffea00",}} />
+                <FontAwesomeIcon icon={faStarHalf} flip="horizontal" style={{color: "#ffea00",}} />
+                </span>
+            </div>
             <div>{props.item.star}</div>
         </div>
     )
