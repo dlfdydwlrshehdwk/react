d79b9e81b803d3d5e0622b738b2ebc80b0190dd7
dlfdydwlrshehdwk <fkrmsk147@naver.com>
2023-10-09 16:26:40 +0900

    1

--- shop/src/App.js
+++ shop/src/App.js
@@ -12,16 +12,39 @@ function App() {
   let [item,setItem] = useState(['감자','고구마','당근']);
   let [good,setGood] = useState([0,0,0])
 
-  function goodUp(){
-
-  }
+  let [list , setList] = 
+  useState([
+    {
+      id : 0,
+      name : '아이템1',
+      star : 4.5
+    },
+    {
+      id : 1,
+      name : '아이템2',
+      star : 3.5
+    },
+    {
+      id : 2,
+      name : '아이템3',
+      star : 2.1
+    },
+  ])
+  
 
   return (
     <div className="App">
       <Header />
-      <div className='container'>
-        <List />
-        <List />
+      <div id='container'>
+        <div className='content-list'>
+          {
+            list.map((x,i)=>{
+              return (
+                <List item={x} key={i}/>
+              )
+            })
+          }
+        </div>
       </div>
       <Footer />
     </div>
--- shop/src/components/component/List.js
+++ shop/src/components/component/List.js
@@ -4,18 +4,10 @@ import { useEffect, useState } from 'react'
 import img1 from '../../assets/images/item1.PNG'
 
 
-function List () {
+function List (props) {
     let [star,setStar] = useState(0)
-
     useEffect(()=>{
-        let star = document.querySelectorAll('.star > li');
-        console.log(star)
-        star.forEach((ele,idx) => {
-            ele.addEventListener('click',function(){
-                console.log(idx)
-                setStar(idx + 1)
-            })
-        });
+        console.log(props)
     })
 
     return(
@@ -24,13 +16,8 @@ function List () {
                 <img src={img1}></img>
             </div>
             <ul className='star'>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
             </ul>
-            <div>{star}</div>
+            <div>{props.item.star}</div>
         </div>
     )
 }
