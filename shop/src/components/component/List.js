45eafe4cd7ee924459cbe1f3009c9966335bbcbe
dlfdydwlrshehdwk <118067728+dlfdydwlrshehdwk@users.noreply.github.com>
2024-01-05 10:50:40 +0900

    Revert "2"
    This reverts commit b4a03b2789187df87d44732e563b5370e7038dd8.

--- shop/src/components/component/List.js
+++ shop/src/components/component/List.js
@@ -1,9 +1,9 @@
 import { useEffect, useState } from 'react'
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import { faStarHalf } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStarHalf } from "@fortawesome/free-solid-svg-icons";
 // 이미지태그를 쓸거면 img태그에서 경로를 지정하는게 아닌
 // import 를 사용하던가 public에 이미지를 놓고 경로를 지정해야함 
-// import img1 from '../../assets/images/item1.PNG'
+import img1 from '../../assets/images/item1.PNG'
 
 
 function List (props) {
@@ -15,12 +15,12 @@ function List (props) {
     return(
         <div className='list'>
             <div>
-                {/* <img src={img1}></img> */}
+                <img src={img1}></img>
             </div>
             <div className='star'>
                 <span>
-                {/* <FontAwesomeIcon icon={faStarHalf} style={{color: "#ffea00",}} /> */}
-                {/* <FontAwesomeIcon icon={faStarHalf} flip="horizontal" style={{color: "#ffea00",}} /> */}
+                <FontAwesomeIcon icon={faStarHalf} style={{color: "#ffea00",}} />
+                <FontAwesomeIcon icon={faStarHalf} flip="horizontal" style={{color: "#ffea00",}} />
                 </span>
             </div>
             <div>{props.item.star}</div>
